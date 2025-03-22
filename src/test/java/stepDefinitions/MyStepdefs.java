6c845a8bee5a64471aa484bc5c49eae161380754
EwaAL <ewa_almkvist@hotmail.com>
2025-03-22 18:43:07 +0100

    Har skapat scenario för att skapa ett konto. Har skapat tillhörande stepdefinitions-fil.

---
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -0,0 +1,64 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("I am at https:\\/\\/membership.basketballengland.co.uk\\/NewSupporterAccount")
+    public void iAmAtHttpsMembershipBasketballenglandCoUkNewSupporterAccount() {
+    }
+
+    @When("I fill in date of birth")
+    public void iFillInDateOfBirth() {
+    }
+
+    @And("I fill in first name")
+    public void iFillInFirstName() {
+    }
+
+    @And("I fill in last name")
+    public void iFillInLastName() {
+    }
+
+    @And("I fill in an email address")
+    public void iFillInAnEmailAddress() {
+    }
+
+    @And("I confirm the Email Address")
+    public void iConfirmTheEmailAddress() {
+    }
+
+    @And("I type a password")
+    public void iTypeAPassword() {
+    }
+
+    @And("I retype the password")
+    public void iRetypeThePassword() {
+    }
+
+    @And("I describe myself as a fan")
+    public void iDescribeMyselfAsAFan() {
+    }
+
+    @And("I agree to the terms and conditions")
+    public void iAgreeToTheTermsAndConditions() {
+    }
+
+    @And("I am over {int} years old")
+    public void iAmOverYearsOld(int arg0) {
+    }
+
+    @And("I agree to the code of ethics and conduct")
+    public void iAgreeToTheCodeOfEthicsAndConduct() {
+    }
+
+    @And("I click the button")
+    public void iClickTheButton() {
+    }
+
+    @Then("an account will be created")
+    public void anAccountWillBeCreated() {
+    }
+}
